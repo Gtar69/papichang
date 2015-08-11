0aca5df122540678a13d5a6544ac1b4324eeabc3
= <=>
2015-08-11 08:51:19 +0000

    local and internet kds queue

--- app/controllers/kds/api_controller.rb
+++ app/controllers/kds/api_controller.rb
@@ -1,12 +1,12 @@
 class Kds::ApiController < ApplicationController
 
   def queue_status
-    queues = WaitingQueue.all.limit(6)  
-    #adding queue process 
+    queues = WaitingQueue.all
+    #adding queue process
     #list = Redis::List.new("kds_queue")
-    waiting_orders = [] 
+    waiting_orders = []
     queues.each do |queue|
-      order = Order.find_by_id(queue.order_id) 
+      order = Order.find_by_id(queue.order_id)
       waiting_orders << { order_id: order.id, created_at: order.created_at,
                           delivery_method: order.delivery_method, match_method: order.match_method,
                           aasm_state: order.aasm_state, order_items: order.get_items}
@@ -17,7 +17,35 @@ class Kds::ApiController < ApplicationController
     render json: { waiting_orders: waiting_orders }
   end
 
-  #queue should be 兩種
+  #inner queue代表在店內交易的訂單
+  def local_queue
+    queues = WaitingQueue.all
+    #adding queue process
+    #list = Redis::List.new("kds_queue")
+    waiting_orders = []
+    queues.each do |queue|
+      order = Order.find_by_id(queue.order_id)
+      if order.match_method = "local"
+      waiting_orders << { order_id: order.id, created_at: order.created_at,
+                          delivery_method: order.delivery_method, match_method: order.match_method,
+                          aasm_state: order.aasm_state, order_items: order.get_items}
+      end
+    render json: { waiting_orders: waiting_orders }
+  end
 
+  def internet_queue
+    queues = WaitingQueue.all
+    #adding queue process
+    #list = Redis::List.new("kds_queue")
+    waiting_orders = []
+    queues.each do |queue|
+      order = Order.find_by_id(queue.order_id)
+      if order.match_method = "internet"
+      waiting_orders << { order_id: order.id, created_at: order.created_at,
+                          delivery_method: order.delivery_method, match_method: order.match_method,
+                          aasm_state: order.aasm_state, order_items: order.get_items}
+      end
+    render json: { waiting_orders: waiting_orders }
+  end
 
 end
