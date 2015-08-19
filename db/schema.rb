4f229732a8f00274d5a82e84a598a169696e1c1f
= <=>
2015-08-19 04:42:34 +0000

    add name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819035657) do
+ActiveRecord::Schema.define(version: 20150819044207) do
 
   create_table "cart_items", force: true do |t|
     t.integer  "cart_id"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20150819035657) do
     t.string   "phone"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
